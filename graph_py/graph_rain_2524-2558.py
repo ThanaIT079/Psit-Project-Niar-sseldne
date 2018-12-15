e15b31d2f47d5a74be6d939c4ce9a56e1e55fad7
Worachet Nimcharoen <43021938+WorachetNimcharoen@users.noreply.github.com>
2018-12-15 18:30:38 +0700

    Update graph_rain_2524-2558.py

--- graph_py/graph_rain_2524-2558.py
+++ graph_py/graph_rain_2524-2558.py
@@ -1,19 +1,20 @@
 """กราฟข้อมูลน้ำฝน 2524 - 2558"""
-
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     """ดึงข้อมูลน้ำฝนและแสดงผลเป็นกราฟ"""
     url = open(r'2524-2558.csv')
     reader = csv.reader(url)
-    x = list(range(2524, 2559))
+    x = np.arange(2524, 2559)
     y = [int(i[1]) for i in reader]
     aver = sum(y) / len(y)
-    result = [aver for _ in range(len(y))]
+    result = np.average(y)
     plt.bar(x, y)
-    plt.plot(x, result, 'r')
+    plt.plot(x, [result]*len(x), 'r')
     plt.xticks(x, rotation=45)
     plt.text(2558, aver, u'ค่าเฉลี่ยทั้งหมด 1458', fontname='JasmineUPC', fontsize='20')
- 
+    plt.show()
+
 main()
