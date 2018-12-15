9d67e7f8b27cd228d9e590ea22bf94e156d15d44
t <38640424+poohcid@users.noreply.github.com>
2018-12-15 18:30:20 +0700

    Update graph_rain_2524-2558.py

--- graph_py/graph_rain_2524-2558.py
+++ graph_py/graph_rain_2524-2558.py
@@ -1,31 +1,19 @@
-"""
+"""กราฟข้อมูลน้ำฝน 2524 - 2558"""
 
-"""
 import csv
 import matplotlib.pyplot as plt
-import numpy as np
 
-def plot_location_all():
-    """กราฟเส้นของแต่ละภาค"""
-    year = np.arange(2548, 2559)
-    color = ['c', 'green', 'magenta', 'coral', 'royalblue', 'lightskyblue', "gray"]
-    region = ["northern", "northeastern", "central", "western", "southwestern", "southeastern", "average"]
-    data_location10 = [location(i) for i in range(1, 8)]
-    style = 0
-    for i in range(0, 7):
-        plt.bar(year+style, data_location10[i], width=0.1, color=color[i], label=region[i])
-        style += 0.1
-    plt.xticks(year, rotation=45)
-    plt.legend()
-    plt.title(u'กราฟรวมปริมาณน้ำฝนทุกภาคในประเทศไทย ตั้งแต่ปี พ.ศ. 2548 - 2558', fontname='JasmineUPC', fontsize='20')
-    plt.xlabel(u'ปี พ.ศ.', fontname='JasmineUPC', fontsize='20')
-    plt.ylabel(u'ปริมาณน้ำฝน(มม.)', fontname='JasmineUPC', fontsize='20')
-    plt.show()
-
-def location(count):
-    """อ่านข้อมูลแล้วส่งข้อมูลเป็น list ของแต่ละภาค"""
-    url = open(r'2548-2558.csv')
+def main():
+    """ดึงข้อมูลน้ำฝนและแสดงผลเป็นกราฟ"""
+    url = open(r'2524-2558.csv')
     reader = csv.reader(url)
-    return [float(i[count]) for i in reader]
-
-plot_location_all()
+    x = list(range(2524, 2559))
+    y = [int(i[1]) for i in reader]
+    aver = sum(y) / len(y)
+    result = [aver for _ in range(len(y))]
+    plt.bar(x, y)
+    plt.plot(x, result, 'r')
+    plt.xticks(x, rotation=45)
+    plt.text(2558, aver, u'ค่าเฉลี่ยทั้งหมด 1458', fontname='JasmineUPC', fontsize='20')
+ 
+main()
