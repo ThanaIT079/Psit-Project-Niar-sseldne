ef7e8d49f354d6ca73dfc12808bbcf26064512f6
t <38640424+poohcid@users.noreply.github.com>
2018-12-16 19:51:35 +0700

    Update project1.py

--- projeact1/project1.py
+++ projeact1/project1.py
@@ -8,14 +8,16 @@ def plot_all():
     """ดึงข้อมูลน้ำฝนและแสดงผลเป็นกราฟ"""
     url = open(r'2524-2558.csv')
     reader = csv.reader(url)
-    x = list(range(2524, 2559))
+    x = np.arange(2524, 2559)
     y = [int(i[1]) for i in reader]
-    aver = np.average(y)
-    result = [aver for _ in range(len(y))]
+    result = np.average(y)
     plt.bar(x, y)
-    plt.plot(x, result, 'r')
+    plt.plot(x, [result]*len(x), 'r')
     plt.xticks(x, rotation=45)
-    plt.text(2558, aver, u'ค่าเฉลี่ยทั้งหมด 1458', fontname='JasmineUPC', fontsize='20')
+    plt.text(2557, result+4, u'ค่าเฉลี่ยทั้งหมด 1458 มม.', fontname='JasmineUPC', fontsize='20')
+    plt.title(u'กราฟรวมปริมาณน้ำฝนในประเทศไทยเฉลี่ยต่อปี ตั้งแต่ปี พ.ศ. 2524 - 2558', fontname='JasmineUPC', fontsize='20')
+    plt.xlabel(u'ปี พ.ศ.', fontname='JasmineUPC', fontsize='20')
+    plt.ylabel(u'ปริมาณน้ำฝน(มม.)', fontname='JasmineUPC', fontsize='20')
     plt.show()
 
 def plot_location(index, color, locate):
